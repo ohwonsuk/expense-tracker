292ca42b17e06ee2407dfeb621e901dfddc86fd2
ohwonsuk <wsoh51@gmail.com>
2023-11-18 18:51:06 +0900

    new expense form 만들기

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import NewExpense from "./NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 function App() {
@@ -24,7 +25,7 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
